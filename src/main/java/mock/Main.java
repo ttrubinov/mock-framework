c27960d9b5438d96817d51378faeade8b8e92099
MihailCCfit <m.tsukanov@g.nsu.ru>
2024-03-24 21:05:52 +0700

    cringe imports

--- src/main/java/mock/Main.java
+++ src/main/java/mock/Main.java
@@ -1,20 +1,8 @@
 package mock;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import net.bytebuddy.implementation.MethodCall;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.implementation.bind.annotation.*;
-import net.bytebuddy.matcher.ElementMatchers;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.*;
-import java.util.concurrent.Callable;
-
 public class Main {
     public static void main(String[] args) throws Exception {
+
 //        Class<Main> classToMock = Main.class;
 //        ByteBuddyAgent.install();
 //
