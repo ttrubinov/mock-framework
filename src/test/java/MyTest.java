c528ed0efcc62f6d3510c32f99f5488ef1f6535c
ttrubinov <t.trubinov@g.nsu.ru>
2024-03-26 22:38:20 +0700

    Good tests

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,24 +2,58 @@ import mock.core.ObjectMock;
 import mock.matchers.ArgumentsMatcher;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
 
     @Test
-    void myTest() throws Exception {
-        try (var mock = ObjectMock.mockStatic(MyClass.class)) {
-//            MyClass.world();
-//            System.out.println(mock);
-            mock.when(MyClass.world(5)).thenReturn("ABOBA");
-            System.out.println(MyClass.world(5));
-        }
-        System.out.println(MyClass.world(5));
-//        try (MockedStatic<MyClass> utilities = Mockito.mockStatic(MyClass.class)) {
-//            utilities.when(() -> MyClass.world("5"))
-//                    .thenReturn(Arrays.asList(10, 11, 12));
-//        }
+    void mockTest() {
+        var mock = ObjectMock.mock(DummyClass.class);
+        ObjectMock.when(mock.plus(3, 2)).thenReturn(1);
+        assertEquals(1, mock.plus(3, 2));
+    }
 
-//        var a = ObjectMock.mock(MyClass.class);
-//        ObjectMock.when(a.world()).thenReturn("ABOBA");
-//        System.out.println(a.world());
+    @Test
+    void staticMockTest() {
+        try (var mock = ObjectMock.mockStatic(DummyClass.class)) {
+            mock.when(DummyClass.printNumber(5)).thenReturn("Mock number");
+            assertEquals("Mock number", DummyClass.printNumber(5));
+        }
+        assertEquals("5", DummyClass.printNumber(5));
+
+        try (var mock = ObjectMock.mockStatic(DummyClass.class)) {
+            mock.when(DummyClass.printNumber(5)).thenReturn("Second mock number");
+            assertEquals("Second mock number", DummyClass.printNumber(5));
+        }
+        assertEquals("5", DummyClass.printNumber(5));
+    }
+
+    @Test
+    void defaultWithStaticMockTest() {
+        var mock = ObjectMock.mock(DummyClass.class);
+        ObjectMock.when(mock.plus(3, 2)).thenReturn(1);
+        assertEquals(1, mock.plus(3, 2));
+        try (var mock2 = ObjectMock.mockStatic(DummyClass.class)) {
+            mock2.when(DummyClass.printNumber(5)).thenReturn("Mock number");
+            assertEquals("Mock number", DummyClass.printNumber(5));
+        }
+        assertEquals("5", DummyClass.printNumber(5));
+        assertEquals(1, mock.plus(3, 2));
+    }
+
+    @Test
+    void matchersTest() {
+        try (var mock = ObjectMock.mockStatic(DummyClass.class)) {
+            mock.when(DummyClass.printNumber(ArgumentsMatcher.anyInt())).thenReturn("Mock number");
+            assertEquals("Mock number", DummyClass.printNumber(5));
+            assertEquals("Mock number", DummyClass.printNumber(3));
+        }
+        assertEquals("5", DummyClass.printNumber(5));
+        assertEquals("3", DummyClass.printNumber(3));
+
+        var mock = ObjectMock.mock(DummyClass.class);
+        ObjectMock.when(mock.plus(ArgumentsMatcher.anyInt(), ArgumentsMatcher.anyInt())).thenReturn(1);
+        assertEquals(1, mock.plus(3, 2));
+        assertEquals(1, mock.plus(5, 10));
     }
 }
