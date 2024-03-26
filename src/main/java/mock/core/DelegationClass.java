7669dc2de32474a424963048315b8a2a19051961
ttrubinov <t.trubinov@g.nsu.ru>
2024-03-26 16:13:45 +0700

    refactoring

--- src/main/java/mock/core/DelegationClass.java
+++ src/main/java/mock/core/DelegationClass.java
@@ -11,36 +11,35 @@ import java.util.*;
 import java.util.concurrent.Callable;
 
 public class DelegationClass {
-        public DelegationClass() {
-        }
+    public DelegationClass() {
+    }
 
-        static final List<Object> lastArguments = new ArrayList<>();
-        static final List<Method> calledMethod = new ArrayList<>();
-        static Method lastCalledMethod = null;
-        static final List<Method> calledStaticMethods = new ArrayList<>();
-        static final Set<Method> originalMethods = new HashSet<>();
+    static final List<Object> lastArguments = new ArrayList<>();
+    static final List<Method> calledMethod = new ArrayList<>();
+    static Method lastCalledMethod = null;
+    static final List<Method> calledStaticMethods = new ArrayList<>();
+    static final Set<Method> originalMethods = new HashSet<>();
     static Callable<?> lastPossibleCall;
     static final String message = "default";
 
-        public static @RuntimeType Object ag(@AllArguments Object[] objects,
-                                             @Origin Method method,
-                                             @SuperCall Callable<?> callable)
-        {
-            lastArguments.clear();
-            lastCalledMethod = method;
-            calledMethod.add(method);
-            lastPossibleCall = callable;
-            if (Modifier.isStatic(method.getModifiers())){
-                if (originalMethods.contains(method)) { //TODO: mb use this for turn back redefining
-                    try {
-                        return method.invoke(objects);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
+    public static @RuntimeType Object ag(@AllArguments Object[] objects,
+                                         @Origin Method method,
+                                         @SuperCall Callable<?> callable) {
+        lastArguments.clear();
+        lastCalledMethod = method;
+        calledMethod.add(method);
+        lastPossibleCall = callable;
+        if (Modifier.isStatic(method.getModifiers())) {
+            if (originalMethods.contains(method)) { //TODO: mb use this for turn back redefining
+                try {
+                    return method.invoke(objects);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-                calledStaticMethods.add(method);
             }
-            lastArguments.addAll(Arrays.stream(objects).toList());
-            return null;
+            calledStaticMethods.add(method);
         }
+        lastArguments.addAll(Arrays.stream(objects).toList());
+        return null;
     }
+}
