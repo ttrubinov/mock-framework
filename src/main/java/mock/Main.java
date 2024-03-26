ffa35b725ac9220d9556a012ebe1e062e2b0103a
ttrubinov <t.trubinov@g.nsu.ru>
2024-03-26 22:40:44 +0700

    Delete useless code

--- src/main/java/mock/Main.java
+++ src/main/java/mock/Main.java
@@ -4,38 +4,6 @@ import mock.core.ObjectMock;
 import mock.matchers.ArgumentsMatcher;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        var mock = ObjectMock.mock(MyClass.class);
-        mock.aboba();
-
-        ObjectMock.when(mock.inc(ArgumentsMatcher.anyInt())).thenReturn(-1);
-        ObjectMock.when(mock.inc(0)).thenReturn(2);
-        System.out.println(mock.inc(0));
-        System.out.println(mock.inc(1));
-        System.out.println(mock.inc(-1));
-//        ObjectMock.when(MyClass.incc(0)).thenReturn(2);
-
-
-//        ObjectMock.when(() -> mock.some(anyInt(), anyInt())).thenThrow(new Exception("no zero"));
-//        ObjectMock.when(() -> mock.some(eq(0), anyInt())).thenThrow(new Exception("first 0"));
-//        ObjectMock.when(() -> mock.some(anyInt(), eq(0))).thenThrow(new Exception("second 0"));
-//        ObjectMock.when(() -> mock.some(eq(0), eq(0))).thenThrow(new Exception("double zero"));
-
-
-//        Class<Main> classToMock = Main.class;
-//        ByteBuddyAgent.install();
-//
-//        Main obj = new ByteBuddy().subclass(classToMock)
-//                .method(ElementMatchers.named("sum"))
-//                .intercept(MethodDelegation.to(MyClass.class).andThen(MethodCall.call(() -> {
-//
-//                    return res;
-//                })))
-//                .make()
-//                .load(classToMock.getClassLoader()
-//                        , ClassReloadingStrategy.fromInstalledAgent()
-//                ).getLoaded().getDeclaredConstructor().newInstance();
-
+    public static void main(String[] args) {
     }
-
 }
